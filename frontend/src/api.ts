626422cfa651880476c9bfbb37633f24277b89aa
cxahal <ahal@computas.com>
2025-06-20 11:15:11 +0200

    Tried to add api key to frontend and require backend api key

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,11 +1,21 @@
 import { User, Experience } from "./types/types";
 
 const API_URL = import.meta.env.VITE_BACKEND_API_URL as string;
+const API_KEY = import.meta.env.VITE_BACKEND_API_KEY as string;
+
+const createHeaders = (): HeadersInit => {
+  return {
+    'Content-Type': 'application/json',
+    'X-Frontend-Api-Key': API_KEY
+  };
+};
 
 export const fetchUsers = async (): Promise<User[]> => {
   try {
-    const response = await fetch(`${API_URL}/users`);
-    
+    const response = await fetch(`${API_URL}/users`, {
+          method: 'GET',
+          headers: createHeaders()
+    });
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
@@ -19,7 +29,10 @@ export const fetchUsers = async (): Promise<User[]> => {
 
 export const fetchExperiences = async (): Promise<Experience[]> => {
   try {
-    const response = await fetch(`${API_URL}/experiences`);
+    const response = await fetch(`${API_URL}/experiences`, {
+          method: 'GET',
+          headers: createHeaders()
+    });
     
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
